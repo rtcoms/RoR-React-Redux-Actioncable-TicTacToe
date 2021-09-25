5779e2d0ea8be8cedfbc2b60cfc3bbb1a1ca00f5
Rohit Trivedi <86.rohit@gmail.com>
2021-09-26 01:43:09 +0530

    add yarn

--- app/javascript/packs/components/gamelisting/index.jsx
+++ app/javascript/packs/components/gamelisting/index.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
-const Gamelisting=()=>
-{
+const Gamelisting = () => {
   return (<React.Fragment>
             <h1>Welcome to Games listing</h1>
             <Link to="/app/gameplay/1">Game</Link>
